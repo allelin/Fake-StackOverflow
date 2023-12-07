f8fa4c4015886c598571b9d1a4735c7075c236a8
Xaladow <105936887+Xaladow@users.noreply.github.com>
2023-12-06 19:18:03 -0500

    schema for account

---
+++ server/models/account.js
@@ -0,0 +1,21 @@
+var mongoose  = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AccountSchema = new Schema(
+    {
+        username: {type: String, required: true, unique: true},
+        email: {type: String, required: true, unique: true},
+        passwordHash: {type: String, required: true},
+    }
+);
+
+// Virtual for account's URL
+AccountSchema
+    .virtual('url')
+    .get(function () {
+        return '/account/' + this._id;
+    });
+
+//Export model
+module.exports = mongoose.model('Account', AccountSchema);
