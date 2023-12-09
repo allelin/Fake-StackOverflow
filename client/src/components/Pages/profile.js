5fa052d0c4bd2bed2375d7d7dbea00681d3f7298
Xaladow <105936887+Xaladow@users.noreply.github.com>
2023-12-09 17:23:45 -0500

    testing

---
+++ client/src/components/Pages/profile.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+
+const Profile = ({ account }) => {
+    return (
+        <div className="profile-page">
+            <h1>Profile Page</h1>
+            <div className="profile-field">
+                <label className="profile-label">Name:</label>
+                <span className="profile-value">{account.name}</span>
+            </div>
+            <div className="profile-field">
+                <label className="profile-label">Email:</label>
+                <span className="profile-value">{account.email}</span>
+            </div>
+            <div className="profile-field">
+                <label className="profile-label">Username:</label>
+                <span className="profile-value">{account.username}</span>
+            </div>
+            {/* Add more fields from the account schema as needed */}
+        </div>
+    );
+};
+
+export default Profile;
